72b15863ef45763cbfbfc3bf8c1bf4dd3b9a3900
christinamartinez <christina.m.codes@gmail.com>
2025-08-03 02:05:47 -0700

    Revert "Update mailbox material colors."
    This reverts commit b8a86fe25a2b6b32c5a4e2d614243cb6db1661c5.

--- postcards/src/components/Box.jsx
+++ postcards/src/components/Box.jsx
@@ -8,7 +8,7 @@ import Flag from "./Flag";
 
 export const Box = forwardRef((props, ref) => {
     const group = useRef();
-    const { nodes, materials, animations } = useGLTF("/mailbox_resend.glb");
+    const { nodes, materials, animations } = useGLTF("/mailbox.glb");
     const { actions } = useAnimations(animations, group);
     const [isFlagUp, setIsFlagUp] = useState(false);
 
@@ -340,4 +340,4 @@ export const Box = forwardRef((props, ref) => {
     );
 });
 
-useGLTF.preload("/mailbox_resend.glb");
+useGLTF.preload("/mailbox2.glb");
