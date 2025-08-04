38cc8f3d5a683ec10657fd776e4ff3c86b16d140
christinamartinez <christina.m.codes@gmail.com>
2025-08-03 21:45:24 -0700

    Add new domain to cors.

--- api/index.js
+++ api/index.js
@@ -13,6 +13,7 @@ app.use(
             "http://localhost:5173",
             "https://postcard-demo-6mg5.vercel.app",
             "https://postcard-demo.vercel.app",
+            "https://snailmail.dev",
         ],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
