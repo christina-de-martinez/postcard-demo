b514e0ed93e798f4f4f808c1c4cebadcd8a3f641
christinamartinez <christina.m.codes@gmail.com>
2025-08-04 19:15:24 -0700

    Try fixing CORS issue

--- api/index.js
+++ api/index.js
@@ -14,6 +14,7 @@ app.use(
             "https://postcard-demo-6mg5.vercel.app",
             "https://postcard-demo.vercel.app",
             "https://snailmail.dev",
+            "https://www.snailmail.dev",
         ],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
